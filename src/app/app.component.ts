6cbf976c969c0e2734885b3aa55f3d609234ac1c
Cyril <c.concas.ext@axione.fr>
2024-05-01 23:57:37 +0200

    First solution

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Quete4';
+  isAdmin:boolean=true;
+  color:String=this.isAdmin?"Red":"Black";
 }
